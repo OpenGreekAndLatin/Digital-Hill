ff2151d5210a47093ce2876b726a51c57474b1e2
MMernitz <mernitz@ebus150.informatik.uni-leipzig.de>
2016-06-13 16:12:16 +0200

    Deleted unnecessary files
    Repo moved, deleted unnecessary files.

--- webpage/ancient_alignments/greekalign.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	$('#colorpen').click(function() { //by clicking on the pencil the marked up words shall be turned into red or green
-		$('.markred').toggleClass('red'), $('.markgreen').toggleClass('green'); //clicking it again will remove the class
-	});
-});
